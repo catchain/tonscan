cd71d2984df7884694582dc58c901ea2195265b1
Alexander Shpindler <shpindler.ar@gmail.com>
2023-11-01 22:43:40 +0400

    fix: preventDefault, stopPropagation, lowestBreakpoint to 80

--- src/js/components/UiTouchSlider.vue
+++ src/js/components/UiTouchSlider.vue
@@ -19,7 +19,7 @@
 export default {
   data() {
     return {
-      lowestBreakpoint: 90,
+      lowestBreakpoint: 80,
       averageBreakpoint: 50,
       fullScreenBreakpoint: 0,
       startY: 0,
@@ -30,16 +30,22 @@ export default {
   },
   methods: {
     onTouchStart(e) {
+      e.preventDefault();
+      e.stopPropagation();
       this.startY = e.touches[0].clientY;
     },
     onTouchMove(e) {
+      e.preventDefault();
+      e.stopPropagation();
       this.currentY = e.touches[0].clientY;
       const diff = ((this.startY - this.currentY) / window.innerHeight) * 100;
       this.top = Math.min(Math.max(this.top - diff, this.fullScreenBreakpoint), this.lowestBreakpoint);
       this.dragDirection = this.currentY - this.startY < 0 ? "top" : "bottom";
       this.startY = this.currentY;
     },
-    onTouchEnd() {
+    onTouchEnd(e) {
+      e.preventDefault();
+      e.stopPropagation();
       if (this.dragDirection === "bottom") {
         if (this.top < this.averageBreakpoint) {
           this.top = this.averageBreakpoint;
